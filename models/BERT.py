ecd251dfe5024a01eacfe2e740cf895fa3634930
AChung1020 <achun46@emory.edu>
2023-11-29 14:49:59 -0500

    log directory now works

--- models/BERT.py
+++ models/BERT.py
@@ -1,3 +1,4 @@
+
 from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments, DataCollatorWithPadding
 from datasets import load_dataset
 import torch
@@ -31,13 +32,13 @@ tokenized_valid.set_format(type="torch", columns=["input_ids", "attention_mask",
 # tokenized_test.set_format(type="torch", columns=["input_ids", "attention_mask", "token_type_ids", "labels"])
 
 training_args = TrainingArguments(
-    output_dir="./results",
+    output_dir="results",
     num_train_epochs=3,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=64,
     warmup_steps=500,
     weight_decay=0.01,
-    logging_dir='./logs',
+    logging_dir='logs',
     logging_steps=10,
     evaluation_strategy="steps",
     save_steps=10_000,
@@ -59,3 +60,4 @@ model.save_pretrained("./BERT_trained_removed")
 tokenizer.save_pretrained("./BERT_trained_removed")
 
 # results = trainer.evaluate(tokenized_test)
+
