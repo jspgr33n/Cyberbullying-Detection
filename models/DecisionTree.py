9953339dd77e1cec5dfcc8eae5a9a1bc52549c74
j-park33 <jamesjinseok@gmail.com>
2023-11-30 10:19:13 -0500

    made some changes and added new ppt presentation and proposal

--- models/DecisionTree.py
+++ models/DecisionTree.py
@@ -4,13 +4,13 @@ from gensim.models import Word2Vec
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report
-from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
 
 # Load the dataset
 df = pd.read_csv('../data/removed_knn_csv.csv')
 
-print(df.head)
-# Assuming 'text' column contains preprocessed text and 'label' column contains labels
+# Assuming 'tweet_text' column contains preprocessed text and 'cyberbullying_type' column contains labels
 df['tweet_text'] = df['tweet_text'].fillna('')
 
 texts = df['tweet_text'].apply(lambda x: x.split())  # Splitting preprocessed text into words
@@ -36,23 +36,58 @@ X = np.nan_to_num(X)
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Define the parameter grid for the grid search
-param_grid = {
-    'min_samples_leaf': range(1, 101),
-    'max_depth': range(1, 101)
-}
+# Best parameters without performing grid search
+best_params = {'max_depth': 9, 'min_samples_leaf': 22}
 
-# Initialize Decision Tree classifier
-dt_classifier = DecisionTreeClassifier()
+# Initialize Decision Tree classifier with the best parameters
+best_model = DecisionTreeClassifier(**best_params)
+
+# Fit the model on the training data
+best_model.fit(X_train, y_train)
+
+# Evaluate the model
+predictions = best_model.predict(X_test)
+print(classification_report(y_test, predictions))
+
+# Use your best Decision Tree model for prediction
+y_pred_prob = best_model.predict_proba(X_test)[:, 1]
+
+# Compute ROC curve
+fpr, tpr, _ = roc_curve(y_test, y_pred_prob)
+
+# Calculate AUROC
+roc_auc = auc(fpr, tpr)
+
+# Plot ROC curve
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve for DT with Best Params')
+plt.legend(loc='lower right')
+plt.show()
+
+
+# # Define the parameter grid for the grid search
+# param_grid = {
+#     'min_samples_leaf': range(1, 101),
+#     'max_depth': range(1, 101)
+# }
+
+# # Initialize Decision Tree classifier
+# dt_classifier = DecisionTreeClassifier()
 
 # Perform grid search using cross-validation with progress printing
 grid_search = GridSearchCV(dt_classifier, param_grid, cv=5, scoring='accuracy', verbose=2, n_jobs=-1)
 grid_search.fit(X_train, y_train)
 
-# Print the best parameters
-print("Best Parameters:", grid_search.best_params_)
+# # Print the best parameters
+# print("Best Parameters:", grid_search.best_params_)
 
-# Evaluate the model with the best parameters
-best_model = grid_search.best_estimator_
-predictions = best_model.predict(X_test)
-print(classification_report(y_test, predictions))
+# # Evaluate the model with the best parameters
+# best_model = grid_search.best_estimator_
+# predictions = best_model.predict(X_test)
+# print(classification_report(y_test, predictions))
--- models/LogisticRegression.py
+++ models/LogisticRegression.py
@@ -1,10 +1,13 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, GridSearchCV
 from gensim.models import Word2Vec
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import classification_report, roc_curve, auc
+
 
 # Get the current working directory, should be within models folder for code to work properly
 current_directory = os.getcwd()
@@ -14,7 +17,7 @@ parent_directory = os.path.dirname(current_directory)
 
 # Define the path to the data folder and the dataset file
 data_folder = os.path.join(parent_directory, 'data')
-data_file = os.path.join(data_folder, 'updated_cyberbullying_data_word2vec_embedding.csv')
+data_file = os.path.join(data_folder, 'removed_knn_csv.csv')
 df = pd.read_csv(os.path.join(data_folder, data_file))
 
 # Split the data into training and testing sets
@@ -38,25 +41,25 @@ xTrain_embeddings = [compute_doc_embedding(text, word2vec_model) for text in xTr
 xTest_embeddings = [compute_doc_embedding(text, word2vec_model) for text in xTest]
 
 # Define hyperparameters for grid search
-param_grid = {
-    'C': [0.001, 0.01, 0.1, 1, 10, 100],  # Regularization strength
-    'penalty': ['l1', 'l2'],  # Penalty type (L1 or L2)
-    'solver': ['liblinear', 'newton-cg', 'lbfgs', 'sag', 'saga'],  # Solver
-    'class_weight': [None, 'balanced'],  # Class weights
-    'max_iter': [100, 200, 300]  # Maximum iterations
-}
+# param_grid = {
+#     'C': [0.001, 0.01, 0.1, 1, 10, 100],  # Regularization strength
+#     'penalty': ['l1', 'l2'],  # Penalty type (L1 or L2)
+#     'solver': ['liblinear', 'newton-cg', 'lbfgs', 'sag', 'saga'],  # Solver
+#     'class_weight': [None, 'balanced'],  # Class weights
+#     'max_iter': [100]  # Maximum iterations
+# }
 
-# Create a Logistic Regression model
-logistic_regression = LogisticRegression()
+# # Create a Logistic Regression model
+# logistic_regression = LogisticRegression()
 
-# Perform grid search with cross-validation and progress printing
-grid_search = GridSearchCV(estimator=logistic_regression, param_grid=param_grid, cv=5, scoring='accuracy', verbose=2, n_jobs=-1)
+# # Perform grid search with cross-validation and progress printing
+# grid_search = GridSearchCV(estimator=logistic_regression, param_grid=param_grid, cv=5, scoring='accuracy', verbose=2, n_jobs=-1)
 
-# Fit the grid search to your training data
-grid_search.fit(xTrain_embeddings, yTrain)
+# # Fit the grid search to your training data
+# grid_search.fit(xTrain_embeddings, yTrain)
 
 # Get the best hyperparameters
-best_params = grid_search.best_params_
+best_params = {'C': 100, 'class_weight': None, 'max_iter': 100, 'penalty': 'l2', 'solver': 'liblinear'}
 
 # Create a logistic regression model with the best hyperparameters
 best_logistic_regression = LogisticRegression(**best_params)
@@ -76,3 +79,41 @@ print(f"Best Hyperparameters: {best_params}")
 print(f"Accuracy: {accuracy:.2f}")
 print("Classification Report:")
 print(classification_rep)
+
+
+# Train the best model on the document embeddings of the training data
+best_logistic_regression.fit(xTrain_embeddings, yTrain)
+
+# Make predictions on the document embeddings of the testing data
+y_pred = best_logistic_regression.predict(xTest_embeddings)
+
+# Evaluate the best model
+accuracy = accuracy_score(yTest, y_pred)
+classification_rep = classification_report(yTest, y_pred)
+
+# Display evaluation metrics and classification report
+print(f"Best Hyperparameters: {best_params}")
+print(f"Accuracy: {accuracy:.2f}")
+print("Classification Report:")
+print(classification_rep)
+
+# Calculate the probabilities for positive class (class 1)
+y_pred_prob = best_logistic_regression.predict_proba(xTest_embeddings)[:, 1]
+
+# Compute ROC curve
+fpr, tpr, _ = roc_curve(yTest, y_pred_prob)
+
+# Calculate AUROC
+roc_auc = auc(fpr, tpr)
+
+# Plot ROC curve
+plt.figure(figsize=(8, 6))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve for LR with Best Params')
+plt.legend(loc='lower right')
+plt.show()
